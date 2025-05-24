160de4804a70899aa840862dafd8fb9c0e2d952a
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-05-24 12:04:52 -0500

    moved buttons around and added icons to buttons

--- CodeCracker/src/App/Guess/Guess.jsx
+++ CodeCracker/src/App/Guess/Guess.jsx
@@ -1,6 +1,6 @@
 import styles from './Guess.module.css';
 
-export default function Guess({guessCount, handleClick, correct, guess, setGuess}) {
+export default function Guess({guessCount, handleClick, correct, guess, setGuess, playAgain, giveUp}) {
 
     const keyDownHandler = (e) => {
         if (e.key === 'Enter') {
@@ -10,9 +10,11 @@ export default function Guess({guessCount, handleClick, correct, guess, setGuess
 
     return (
         <div className={styles.guess}>
-            <i className={`fa-solid fa-${correct ? 'check' : 'xmark'}`} style={{display: guessCount>0 ? 'block' : 'none', color: correct ? 'green' : 'red'}}></i>
-            <button onClick={() => handleClick(false)}>{correct ? 'Play Again' : 'Guess'}</button>
+            <button onClick={giveUp} style={{display: correct ? 'none' : 'flex' }}><i class="fa-solid fa-thumbs-down"></i><span>Give Up</span></button>
+            <button onClick={playAgain}><i class="fa-solid fa-rotate-right"></i><span>Restart</span></button>
+            <button onClick={() => handleClick(false)} style={{display: correct ? 'none' : 'flex' }}><i class="fa-solid fa-question"></i><span>Guess</span></button>
             <input type="text" value={guess} onChange={(e) => setGuess(e.target.value)} onKeyDown={keyDownHandler} readOnly={correct}/>
+            <i className={`fa-solid fa-${correct ? (correct===1 ? 'check' : 'ban') : 'xmark'} icon`} style={{color: guessCount>0 ? (correct ? (correct===1 ? 'green' : 'yellow') : 'red') : 'transparent'}}></i>
         </div>
     )
 }
